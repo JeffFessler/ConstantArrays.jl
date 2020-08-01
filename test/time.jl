4931a4c69356c91f6baf8eb2e0b873d6c618a741
Jeff Fessler <fessler@umich.edu>
2020-08-01 11:49:22 -0400

    Timing tests vs FillArrays

--- test/time.jl
+++ test/time.jl
@@ -2,6 +2,7 @@
 # some timing tests
 
 using ConstantArrays
+using FillArrays: Ones
 using BenchmarkTools
 
 dim = (2^8,2^8,2^4)
@@ -10,41 +11,51 @@ mask0 = trues(dim)
 x = randn(dim)
 y = similar(x)
 
-mask1 = ConstantArray(dim)
+maskc = ConstantArray(dim)
+maskf = Ones{Bool}(dim)
 
-@assert mask0 == mask1 
+@assert mask0 == maskc == maskf
 
-function dottimes(y, mask, x)
+function dottimes(y, mask, x) # for timing .*
 	return y .= mask .* x
 end
 
-function indexer(y, mask, x)
+function indexer(y, mask, x) # getindex!
 	y[mask] .= vec(x)
 end
 
-function masker(y, mask, x)
+function masker(y, mask, x) # getindex
 	vec(y) .= x[mask]
 end
 
+# FillArrays is even slower than base here - it needs an appropriate getindex
 if true
-	@btime dottimes($y, $mask0, $x)
-	@btime dottimes($y, $mask1, $x) # about 2× faster!
-end
-
-if true
-#	indexer(y, mask0, x)
-	@btime indexer($y, $mask0, $x)
-	@btime indexer($y, $mask1, $x) # 3× more memory and slower :(
-end
-
-if true
-	@assert masker(y, mask1, x) == vec(x)
-#	masker(y, mask1, x)
+	@assert masker(y, maskc, x) == vec(x)
+#	masker(y, maskc, x)
 	@btime masker($y, $mask0, $x) # 2 ms 6 alloc 8MiB
-	@btime masker($y, $mask1, $x) # 3 ms 4 alloc 8MiB => was 1.5× slower :(
+	@btime masker($y, $maskc, $x) # 3 ms 4 alloc 8MiB => was 1.5× slower :(
 								# before overloaded getindex
 								# then became:
 								# 0.66 ms 4 alloc 160 byte => 3× faster!
+	@btime masker($y, $maskf, $x) # 2.6 ms (4 allocations: 8.00 MiB)
+end
+
+# todo: work on a getindex for Fill
+# Base.getindex(x::AbstractArray{T,D}, mask::Fill{Bool,D,todo}) where {T,D} =mask.value ? vec(x) : ones(T, 0)
+
+
+# todo: anyway to speed up this one?
+if true
+#	indexer(y, mask0, x)
+	@btime indexer($y, $mask0, $x) # 2 ms (11 allocations: 8.00 MiB)
+	@btime indexer($y, $maskc, $x) # 5 ms (6 allocations: 24.00 MiB) :(
+	@btime indexer($y, $maskf, $x) # 5 ms (6 allocations: 24.00 MiB) :(
+end
+
+if true
+	@btime dottimes($y, $mask0, $x) # 960 μs
+	@btime dottimes($y, $maskc, $x) # 630 μs
+	@btime dottimes($y, $maskf, $x) # 630 μs
 end
 
 nothing
